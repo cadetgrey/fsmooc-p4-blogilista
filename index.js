6408578b390473d97168d91433b42279e73e1329
cadetgrey <maija.raudaskoski@gmail.com>
2018-11-24 23:01:42 +0200

    initial commit, basic get/post functionality ok

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs.js')
+
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
